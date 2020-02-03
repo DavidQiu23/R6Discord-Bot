d325157c3e145308dcab4a1eb43272038c7203dc
Daivd <suzukiami607@gmail.com>
2020-02-03 10:37:58 +0800

    尋找Expecting value: line 1 column 1 (char 0) 問題

--- main.py
+++ main.py
@@ -172,6 +172,9 @@ async def vsoperator(ctx,user1,user2,operator):
 async def player(ctx,user):
     try:
         print("get_player before")
+        print(auth.key)
+        print(auth.appid)
+        print(auth.sessionid)
         player = await auth.get_player(user,r6.Platforms.UPLAY)
         print(player)
         await player.load_general()
