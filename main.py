8e6a5eefd8ac0bae01e31ce794927c62731d5cf4
Daivd <suzukiami607@gmail.com>
2020-08-13 17:03:57 +0800

    test rank function

--- main.py
+++ main.py
@@ -228,10 +228,9 @@ async def rank(ctx,user):
     try:
         player = await auth.get_player(user,r6.Platforms.UPLAY)
 
-        await player.load_rank(r6.RankedRegions.ASIA)
         data = await player.get_rank(r6.RankedRegions.ASIA)
-        await ctx.send(player.ranks)
-        await ctx.send(data)
+
+        await ctx.send(data.wins + " "+data.losses+" "+data.mmr+" "+data.max_mmr)
     except Exception as error:
         await ctx.send(error)
 
